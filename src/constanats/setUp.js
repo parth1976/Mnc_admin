38456399e7ee233ae74e93c453eb2f3a7fc40d4f
Jenil Ghevariya <jenilghevariya000@gmail.com>
2025-03-29 21:29:23 +0530

    build

--- src/constanats/setUp.js
+++ src/constanats/setUp.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = "http://localhost:3001"
-// export const BASE_URL = "https://back.starlightgroup.in"
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "https://back.multinetworkingcompany.com"
