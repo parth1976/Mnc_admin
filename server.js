580d4b11c6e5f47c7e50e1350a82d13e1dcade73
Jenil Ghevariya <jenilghevariya000@gmail.com>
2025-03-29 21:02:47 +0530

    add 3030 port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build/index.html"));
 });
 
-const PORT = 5000;
+const PORT = 3030;
 
 console.log("server started on port:", PORT);
 app.listen(PORT);
